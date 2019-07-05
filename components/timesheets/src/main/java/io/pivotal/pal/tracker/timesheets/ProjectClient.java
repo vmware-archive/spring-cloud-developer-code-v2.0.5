a5751eb5e0888fa1c2827b07116ce40a97cf965a
Bill Kable <bkable@pivotal.io>
2019-07-05 18:01:06 -0500

    hystrix-solution

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,8 +1,16 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ProjectClient {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+    final Map<Long, ProjectInfo> projectInfoCache;
 
     private final RestOperations restOperations;
     private final String endpoint;
@@ -10,9 +18,22 @@ public class ProjectClient {
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations = restOperations;
         this.endpoint = registrationServerEndpoint;
+        this.projectInfoCache = new ConcurrentHashMap<>();
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        logger.info("Obtained and cached projectInfo for projectId '{}'", projectId);
+
+        ProjectInfo projectInfo = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        projectInfoCache.put(projectId,projectInfo);
+
+        return projectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        logger.info("Fell back to using cached project lookup for projectId '{}'", projectId);
+
+        return projectInfoCache.get(projectId);
     }
 }
