699cd82babc08695b77863fda5223d0de9fcde7b
Bill Kable <bkable@pivotal.io>
2019-07-05 18:01:06 -0500

    hystrix-isolation-strategies-start.orig

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/TimesheetsApp.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/TimesheetsApp.java
@@ -3,11 +3,9 @@ package io.pivotal.pal.tracker.timesheets;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestOperations;
 
 import java.util.TimeZone;
@@ -21,21 +19,6 @@ public class TimesheetsApp {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(TimesheetsApp.class, args);
-    }
-
-    // NOTE: We are ignoring the Rest Support Rest Operations
-    // given we must apply RestTemplateBuilder via Spring Boot
-    // Dependencies
-    @Primary
-    @Bean
-    RestOperations restOperations(
-            @Value("${project.client.connect.timeout.ms}") int connectTimeout,
-            @Value("${project.client.read.timeout.ms}") int readTimeout
-    ) {
-        return new RestTemplateBuilder()
-                .setConnectTimeout(connectTimeout)
-                .setReadTimeout(readTimeout)
-                .build();
     }
 
     @Bean
