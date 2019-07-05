fd104b0adadb5b7732de8d9959a77915596ea2dc
Bill Kable <bkable@pivotal.io>
2019-07-05 18:01:07 -0500

    hystrix-stats-aggregation-solution

---
+++ platform-services/turbine-server/src/main/java/io/pivotal/pal/tracker/turbineserver/TurbineServerApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.pal.tracker.turbineserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableTurbine
+@SpringBootApplication
+public class TurbineServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineServerApplication.class, args);
+    }
+}
